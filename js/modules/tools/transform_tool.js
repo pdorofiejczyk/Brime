dd98fa46000a51fa3c7619ba6a9585d43702d1a3
Paweł Dorofiejczyk <pawel.dorofiejczyk@gmail.com>
2012-02-24 10:35:58 +0100

    small fix for transform tool

--- js/modules/tools/transform_tool.js
+++ js/modules/tools/transform_tool.js
@@ -15,49 +15,42 @@ var TransformTool = new Class({
       var isLeftClick  = 'which' in e ? e.which == 1 : e.button == 1;
       if (!isLeftClick && !fabric.isTouchSupported) return;
 
-      if (this.isDrawingMode) {
-        this._prepareForDrawing(e);
-
-        // capture coordinates immediately; this allows to draw dots (when movement never occurs)
-        this._captureDrawingPath(e);
-
-        return;
-      }
-
       // ignore if some object is being transformed at this moment
       if (this._currentTransform) return;
 
-      var target = this.getActiveObject() ? this.getActiveObject() : this.findTarget(e),
-          pointer = this.getPointer(e),
-          activeGroup = this.getActiveGroup(), 
-          corner;
+      var target = this.getActiveObject();
+      
+      if(target !== null && target != undefined) {
+        var pointer = this.getPointer(e),
+            activeGroup = this.getActiveGroup(), 
+            corner;
 
+          // determine if it's a drag or rotate case
+          // rotate and scale will happen at the same time
+          this.stateful && target.saveState();
 
-        // determine if it's a drag or rotate case
-        // rotate and scale will happen at the same time
-        this.stateful && target.saveState();
-
-        if (corner = target._findTargetCorner(e, this._offset)) {
-          this.onBeforeScaleRotate(target);
-        }
-
-        this._setupCurrentTransform(e, target);
-
-        var shouldHandleGroupLogic = e.shiftKey && (activeGroup || this.getActiveObject());
-        if (shouldHandleGroupLogic) {
-          this._handleGroupLogic(e, target);
-        }
-        else {
-          if (target !== this.getActiveGroup()) {
-            this.deactivateAll();
+          if (corner = target._findTargetCorner(e, this._offset)) {
+            this.onBeforeScaleRotate(target);
           }
-          this.setActiveObject(target, e);
-        }
 
-      // we must renderAll so that active image is placed on the top canvas
-      this.renderAll();
+          this._setupCurrentTransform(e, target);
 
-      this.fire('mouse:down', { target: target, e: e });
+          var shouldHandleGroupLogic = e.shiftKey && (activeGroup || this.getActiveObject());
+          if (shouldHandleGroupLogic) {
+            this._handleGroupLogic(e, target);
+          }
+          else {
+            if (target !== this.getActiveGroup()) {
+              this.deactivateAll();
+            }
+            this.setActiveObject(target, e);
+          }
+
+        // we must renderAll so that active image is placed on the top canvas
+        this.renderAll();
+
+        this.fire('mouse:down', { target: target, e: e });
+      }
     },
   
   _on_mouse_move: fabric.Canvas.prototype.__onMouseMove
